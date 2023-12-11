ba699015e84bd18228312b32aa161d8382d0c87b
kev1nnnnnnn <kevinbfv@gmail.com>
2023-12-11 18:17:11 -0300

    no audio

--- src/components/atoms/Audio.tsx
+++ src/components/atoms/Audio.tsx
@@ -1,23 +1,14 @@
-import React, { useEffect, useRef } from "react";
-import music from "../../assets/songs/trap-future-bass-royalty-free-music-167020.mp3";
+import React from "react"
+import music from "../../assets/songs/trap-future-bass-royalty-free-music-167020.mp3"
 
-export const Audio: React.FC = () => {
-  const audioRef = useRef<HTMLAudioElement>(null);
 
-  useEffect(() => {
-    // Quando o componente montar, inicie a reprodução automática
-    if (audioRef.current) {
-      audioRef.current.play();
-    }
-  }, []);
-
-  return (
-    <>
-      {/* Adicione a propriedade 'hidden' para esconder o player de áudio */}
-      <audio ref={audioRef} controls hidden>
+export const Audio = ( ) => {
+    return (
+       <>
+       <audio controls>
         <source src={music} type="audio/mp3" />
         Seu navegador não suporta a tag de áudio.
-      </audio>
-    </>
-  );
-};
+        </audio>
+       </>
+    )
+}
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -7,14 +7,14 @@ import Arrecad from "../organs/Arrecad"
 import Testimonials from "../organs/Testimonials"
 import Blogs from "../organs/Blogs"
 import Offers from "../organs/Offers"
-import { Audio } from "../atoms/Audio"
+
 
 
 const Home = () => {
     return (
         <>
             <HeroSection />
-            <Audio />
+            {/* <Audio /> */}
             <Offers />
             <Arrecad />
             {/* <Membership /> 
